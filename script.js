dfd95f29fadd902d5e82f42b5c5830b6ff78afc0
Getlost01 <aagamjain573@gmail.com>
2022-07-16 08:07:16 +0530

    full screen added

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const answer = document.querySelector(`#answer`)
+const answer = document.querySelector(`#answer`);
+const fullScreenToggle = document.querySelector(`#fullScreenToggle`);
+const bodyEle = document.documentElement;
 var btnArr = ["A","B","C","D"];
+
+// -----Prevent right click -------
+document.addEventListener("contextmenu", function (e){
+    e.preventDefault();
+}, false);
+
 
 btnArr.forEach(button => {
     document.querySelector(`#${button}`).addEventListener('click',()=>{
@@ -11,3 +19,17 @@ btnArr.forEach(button => {
         console.log(button);
     });
 });
+
+fullScreenToggle.addEventListener('click',()=>{
+    console.log(document.fullscreenElement)
+    if (document.fullscreenElement) {
+        document.exitFullscreen()
+        .then(() => console.log("Document Exited from Full screen mode"))
+        .catch((err) => console.error(err));
+        fullScreenToggle.innerHTML = `<i class="fa-solid fa-up-right-and-down-left-from-center"></i>`;
+      }
+      else {
+        document.documentElement.requestFullscreen();
+        fullScreenToggle.innerHTML = `<i class="fa-solid fa-down-left-and-up-right-to-center"></i>`;
+      }
+})
